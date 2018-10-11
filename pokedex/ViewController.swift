5a1138e598da790d44a1baab7073c2e6440eff17
Manuel Teixeira <mateixeira@blissapplications.com>
2018-10-11 10:20:53 +0100

    Initial Commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Manuel Teixeira on 11/10/2018.
+//  Copyright © 2018 Manuel Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
